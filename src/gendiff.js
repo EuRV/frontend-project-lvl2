84b23592c57f68459f361b141405f5d7b530248d
EuRV <jugin_omsk@mail.ru>
2022-02-17 02:28:36 +0600

    Add /src/gendiff.js
    Added file and logic gendiff.js.

---
+++ src/gendiff.js
@@ -0,0 +1,28 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import { cwd } from 'process';
+
+const parseFile = (pathToFile) => JSON.parse(readFileSync(path.resolve(cwd(), pathToFile)));
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const data1 = parseFile(pathToFile1);
+  const data2 = parseFile(pathToFile2);
+  const data = { ...data1, ...data2 };
+  const dataKeys = Object.keys(data).sort();
+  const diff = dataKeys.reduce((acc, key) => {
+    if (!(key in data1)) {
+      acc[`+ ${key}`] = data2[key];
+    } else if (!(key in data2)) {
+      acc[`- ${key}`] = data1[key];
+    } else if (data1[key] === data2[key]) {
+      acc[`  ${key}`] = data1[key];
+    } else {
+      acc[`- ${key}`] = data1[key];
+      acc[`+ ${key}`] = data2[key];
+    }
+    return acc;
+  }, {});
+  return JSON.stringify(diff, null, 2).replace(/[",]/g, '');
+};
+
+export default genDiff;
