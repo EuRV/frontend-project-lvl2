ab480e7b3f79caa653330160a61fc870a3cac129
EuRV <jugin_omsk@mail.ru>
2022-02-21 18:59:03 +0600

    Add yaml file test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,8 +8,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('aaa', () => {
+test('check gendiff JSON', () => {
   const diff = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
+  const str = readFile('file-reference.txt');
+  expect(diff).toEqual(str);
+});
+
+test('check gendiff YAML', () => {
+  const diff = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml');
   const str = readFile('file-reference.txt');
   expect(diff).toEqual(str);
 });
