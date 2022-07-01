07939312976e35010a65c6eea5b8205593aeb3c1
EuRV <eugenirovensky@yandex.ru>
2022-07-01 21:55:22 +0600

    feat: implement file comparison logic
    Implement the logic of finding differences for files with nested structures

---
+++ src/getdifference.js
@@ -0,0 +1,22 @@
+const getDifference = (data1, data2) => {
+  const data = { ...data1, ...data2 };
+  const dataKeys = Object.keys(data).sort();
+  return dataKeys.reduce((acc, key) => {
+    if (!(key in data1)) {
+      acc[key] = { key, type: 'added', value: data2[key] };
+    } else if (!(key in data2)) {
+      acc[key] = { key, type: 'removed', value: data1[key] };
+    } else if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+      acc[key] = { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
+    } else if (data1[key] === data2[key]) {
+      acc[key] = { key, type: 'unchanged', value: data1[key] };
+    } else if ((key in data1 && key in data2) && data1[key] !== data2[key]) {
+      acc[key] = {
+        key, type: 'changed', previousValue: data1[key], nextValue: data2[key],
+      };
+    }
+    return acc;
+  }, {});
+};
+
+export default getDifference;
