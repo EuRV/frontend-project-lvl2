4cc305552177f3742b5bd1882a277315847cfba9
EuRV <eugenirovensky@yandex.ru>
2022-07-02 02:31:27 +0600

    feat: implement loading of modules getdifference.js stylish.js
    The logic of comparing files with nested structure has been moved along the path getdifference.js and /formatters/stylish.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,24 +1,12 @@
 import parseFile from './parsers.js';
+import getDifference from './getdifference.js';
+import stylish from './formatters/stylish.js';
 
 const genDiff = (pathToFile1, pathToFile2) => {
   const data1 = parseFile(pathToFile1);
   const data2 = parseFile(pathToFile2);
-  const data = { ...data1, ...data2 };
-  const dataKeys = Object.keys(data).sort();
-  const diff = dataKeys.reduce((acc, key) => {
-    if (!(key in data1)) {
-      acc[`+ ${key}`] = data2[key];
-    } else if (!(key in data2)) {
-      acc[`- ${key}`] = data1[key];
-    } else if (data1[key] === data2[key]) {
-      acc[`  ${key}`] = data1[key];
-    } else {
-      acc[`- ${key}`] = data1[key];
-      acc[`+ ${key}`] = data2[key];
-    }
-    return acc;
-  }, {});
-  return JSON.stringify(diff, null, 2).replace(/[",]/g, '');
+  const result = getDifference(data1, data2);
+  return stylish(result);
 };
 
 export default genDiff;
