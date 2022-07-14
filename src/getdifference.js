cce7dfeb0731c66e5ebc762200fc1bef38ccdba4
EuRV <eugenirovensky@yandex.ru>
2022-07-15 01:23:39 +0600

    refactor: Apply lodash

--- src/getdifference.js
+++ src/getdifference.js
@@ -1,3 +1,6 @@
+import has from 'lodash/has';
+import isObject from 'lodash/isObject';
+
 const getDifference = (data1, data2) => {
   const data = { ...data1, ...data2 };
   const dataKeys = Object.keys(data).sort();
@@ -6,11 +9,11 @@ const getDifference = (data1, data2) => {
       acc[key] = { key, type: 'added', value: data2[key] };
     } else if (!(key in data2)) {
       acc[key] = { key, type: 'removed', value: data1[key] };
-    } else if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+    } else if (isObject(data1[key]) && isObject(data2[key])) {
       acc[key] = { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
     } else if (data1[key] === data2[key]) {
       acc[key] = { key, type: 'unchanged', value: data1[key] };
-    } else if ((data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) && data1[key] !== data2[key]) {
+    } else if ((has(data1, key) && has(data2, key)) && data1[key] !== data2[key]) {
       acc[key] = {
         key, type: 'changed', previosValue: data1[key], nextValue: data2[key],
       };
