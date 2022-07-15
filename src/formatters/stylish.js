d65352ab3a52bc5c3a4524fdee70c4c0e621c8ca
EuRV <eugenirovensky@yandex.ru>
2022-07-15 18:27:38 +0600

    refactor: add the getIndent function
    Replace duplicate code with a function getIndent

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,18 +1,22 @@
+const getIndent = (depth, replacer = ' ') => replacer.repeat(depth * 2);
+
 const getLine = (value, count) => {
   if (typeof value !== 'object' || value === null) {
     return `${value}`;
   }
   const lines = Object.entries(value)
-    .map(([key, val]) => `${' '.repeat(count * 2)}  ${key}: ${getLine(val, count + 2)}`);
+    .map(([key, val]) => `${getIndent(count)}  ${key}: ${getLine(val, count + 2)}`);
   return `{\n${lines.join('\n')}\n${' '.repeat(count * 2 - 2)}}`;
 };
 
 const stringify = {
-  added: (node, depth) => `\n${' '.repeat(depth * 2)}+ ${node.key}: ${getLine(node.value, depth + 2)}`,
-  removed: (node, depth) => `\n${' '.repeat(depth * 2)}- ${node.key}: ${getLine(node.value, depth + 2)}`,
-  unchanged: (node, depth) => `\n${' '.repeat(depth * 2)}  ${node.key}: ${getLine(node.value, depth + 2)}`,
-  changed: (node, depth) => `\n${' '.repeat(depth * 2)}- ${node.key}: ${getLine(node.previosValue, depth + 2)}\n${' '.repeat(depth * 2)}+ ${node.key}: ${getLine(node.nextValue, depth + 2)}`,
-  nested: (node, depth, iter) => `\n${' '.repeat(depth * 2)}  ${node.key}: {${iter(node.children, depth + 2, iter).join('')}\n${' '.repeat(depth * 2)}  }`,
+  added: (node, depth) => `\n${getIndent(depth)}+ ${node.key}: ${getLine(node.value, depth + 2)}`,
+  removed: (node, depth) => `\n${getIndent(depth)}- ${node.key}: ${getLine(node.value, depth + 2)}`,
+  unchanged: (node, depth) => `\n${getIndent(depth)}  ${node.key}: ${getLine(node.value, depth + 2)}`,
+  changed: (node, depth) => [`\n${getIndent(depth)}- ${node.key}: ${getLine(node.previosValue, depth + 2)}`,
+    `\n${getIndent(depth)}+ ${node.key}: ${getLine(node.nextValue, depth + 2)}`,
+  ],
+  nested: (node, depth, iter) => `\n${getIndent(depth)}  ${node.key}: {${iter(node.children, depth + 2, iter).join('')}\n${getIndent(depth)}  }`,
 };
 
 const stylish = (diff) => {
