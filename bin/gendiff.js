35f579e26ad03dd05711e287b47b99dd94f24f17
EuRV <eugenirovensky@yandex.ru>
2022-07-18 01:24:57 +0600

    feat: implement the choice of a formatter
    Add a choice of formatter. Make the stylish formatter by default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,10 @@ const program = new Command();
 program
   .version('1.0.0', '-v, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, opts) => {
+    console.log(genDiff(filepath1, filepath2, opts.format));
   });
 
 program.parse(process.argv);
