1db32c9d7c74005e3deaff0f8430388bc322b300
EuRV <eugenirovensky@yandex.ru>
2022-07-20 02:51:25 +0600

    feat: add test with json formatter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,9 +8,10 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('check gendiff stylish', () => {
+describe('check gendiff', () => {
   const expectedStylish = readFile('current-stylish.txt');
   const expectedPlain = readFile('current-plain.txt');
+  const expectedJson = readFile('current-json.txt');
   test('check the STYLISH formatter with the JSON extension', () => {
     const diff = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish');
     expect(diff).toEqual(expectedStylish);
@@ -24,5 +25,10 @@ describe('check gendiff stylish', () => {
   test('check the PLAIN formatter with the JSON extension', () => {
     const diff1 = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
     expect(diff1).toEqual(expectedPlain);
+  });
+
+  test('check the JSON formatter with the JSON extension', () => {
+    const diff2 = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json');
+    expect(diff2).toEqual(expectedJson);
   });
 });
