704605d3716e161c6775684b36af4f0f8e1dace2
EuRV <eugenirovensky@yandex.ru>
2022-07-22 03:33:51 +0600

    refactor: apply lodash

--- src/getdifference.js
+++ src/getdifference.js
@@ -1,9 +1,13 @@
 import has from 'lodash/has.js';
 import isObject from 'lodash/isObject.js';
+import keys from 'lodash/keys.js';
+import sortBy from 'lodash/sortBy.js';
+import union from 'lodash/union.js';
 
 const getDifference = (data1, data2) => {
-  const data = { ...data1, ...data2 };
-  const dataKeys = Object.keys(data).sort();
+  const data1Key = keys(data1);
+  const data2Key = keys(data2);
+  const dataKeys = sortBy(union(data1Key, data2Key));
   return dataKeys.reduce((acc, key) => {
     if (!(key in data1)) {
       acc[key] = { key, type: 'added', value: data2[key] };
