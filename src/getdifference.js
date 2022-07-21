d69c2e7d907c6ab9d3e0bb9494b4562c9f2bc12b
EuRV <eugenirovensky@yandex.ru>
2022-07-22 04:19:53 +0600

    refactor: change the structure of the getDifference block
    Change dataKeys.reduce to dataKeys.map. Make an output in the form of an array

--- src/getdifference.js
+++ src/getdifference.js
@@ -8,22 +8,23 @@ const getDifference = (data1, data2) => {
   const data1Key = keys(data1);
   const data2Key = keys(data2);
   const dataKeys = sortBy(union(data1Key, data2Key));
-  return dataKeys.reduce((acc, key) => {
-    if (!(key in data1)) {
-      acc[key] = { key, type: 'added', value: data2[key] };
-    } else if (!(key in data2)) {
-      acc[key] = { key, type: 'removed', value: data1[key] };
-    } else if (isObject(data1[key]) && isObject(data2[key])) {
-      acc[key] = { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
-    } else if (data1[key] === data2[key]) {
-      acc[key] = { key, type: 'unchanged', value: data1[key] };
-    } else if ((has(data1, key) && has(data2, key)) && data1[key] !== data2[key]) {
-      acc[key] = {
+  return dataKeys.map((key) => {
+    if (!has(data1, key)) {
+      return { key, type: 'added', value: data2[key] };
+    }
+    if (!has(data2, key)) {
+      return { key, type: 'removed', value: data1[key] };
+    }
+    if (isObject(data1[key]) && isObject(data2[key])) {
+      return { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
+    }
+    if ((has(data1, key) && has(data2, key)) && data1[key] !== data2[key]) {
+      return {
         key, type: 'changed', previosValue: data1[key], nextValue: data2[key],
       };
     }
-    return acc;
-  }, {});
+    return { key, type: 'unchanged', value: data1[key] };
+  });
 };
 
 export default getDifference;
