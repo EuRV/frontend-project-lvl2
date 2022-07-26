58043fe621e9de915db07f35cfebbed4442124a7
EuRV <eugenirovensky@yandex.ru>
2022-07-26 23:09:04 +0600

    refactor: change the plain function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,25 +7,23 @@ const getLine1 = (value) => {
 };
 
 const plain = (diff) => {
-  // console.log(Object.values(diff));
-  const iter = (node, pre) => Object.values(node)
-    .flatMap((val) => {
-      const path = [...pre, val.key].join('.');
-      switch (val.type) {
-        case 'added':
-          return `Property '${path}' was added with value: ${getLine1(val.value)}`;
-        case 'removed':
-          return `Property '${path}' was removed`;
-        case 'unchanged':
-          return [];
-        case 'changed':
-          return `Property '${path}' was updated. From ${getLine1(val.previosValue)} to ${getLine1(val.nextValue)}`;
-        case 'nested':
-          return `${iter(val.children, [path]).join('\n')}`;
-        default:
-          throw new Error(`Type: ${val.type} is undefined`);
-      }
-    });
+  const iter = (node, pre) => node.flatMap((val) => {
+    const path = [...pre, val.key].join('.');
+    switch (val.type) {
+      case 'added':
+        return `Property '${path}' was added with value: ${getLine1(val.value)}`;
+      case 'removed':
+        return `Property '${path}' was removed`;
+      case 'unchanged':
+        return [];
+      case 'changed':
+        return `Property '${path}' was updated. From ${getLine1(val.previosValue)} to ${getLine1(val.nextValue)}`;
+      case 'nested':
+        return `${iter(val.children, [path]).join('\n')}`;
+      default:
+        throw new Error(`Type: ${val.type} is undefined`);
+    }
+  });
   return `${iter(diff, []).join('\n')}`;
 };
 
